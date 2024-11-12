719d00a8fb8bd8f2bafb194cf3d2db26e801678b
Akeeb4316 <akeeb.hossain@ufl.edu>
2024-11-12 01:23:33 -0500

    Updated Function.cs file with necessary changes

--- Function.cs
+++ Function.cs
@@ -1,8 +1,6 @@
 using System.Text;
 using Amazon.Lambda.Core;
 using Newtonsoft.Json;
-using System.Net.Http;
-using System;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
@@ -11,34 +9,29 @@ namespace EsepWebhook;
 
 public class Function
 {
+    
     /// <summary>
-    /// A function that handles a GitHub webhook and sends a message to Slack.
+    /// A simple function that takes a string and does a ToUpper
     /// </summary>
-    /// <param name="input">The event input from GitHub webhook.</param>
-    /// <param name="context">The Lambda context.</param>
-    /// <returns>A response string.</returns>
+    /// <param name="input">The event for the Lambda function handler to process.</param>
+    /// <param name="context">The ILambdaContext that provides methods for logging and describing the Lambda environment.</param>
+    /// <returns></returns>
     public string FunctionHandler(object input, ILambdaContext context)
     {
-        // Log input to CloudWatch
         context.Logger.LogInformation($"FunctionHandler received: {input}");
 
-        // Deserialize GitHub webhook payload into dynamic object
         dynamic json = JsonConvert.DeserializeObject<dynamic>(input.ToString());
-
-        // Prepare payload for Slack with the issue URL
         string payload = $"{{'text':'Issue Created: {json.issue.html_url}'}}";
-
-        // Send POST request to Slack webhook URL
+        
         var client = new HttpClient();
         var webRequest = new HttpRequestMessage(HttpMethod.Post, Environment.GetEnvironmentVariable("SLACK_URL"))
         {
             Content = new StringContent(payload, Encoding.UTF8, "application/json")
         };
-
+    
         var response = client.Send(webRequest);
-
-        // Read and return Slack response
         using var reader = new StreamReader(response.Content.ReadAsStream());
+            
         return reader.ReadToEnd();
     }
 }
--- obj/Release/net8.0/linux-x64/EsepWebhook.AssemblyInfo.cs
+++ obj/Release/net8.0/linux-x64/EsepWebhook.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EsepWebhook")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a8fd3cbe216c6969d8c0846c3130a3bebee61a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("EsepWebhook")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EsepWebhook")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
